528093f299ffc3680592cf3237f5c59c0b35da73
Abdulaziz <154592349+abdulazizmughal@users.noreply.github.com>
2024-03-07 14:56:48 +0500

    Add files via upload

---
+++ syle.js
@@ -0,0 +1,21 @@
+                                //    CHAPTER NO 3
+// Question no 1
+var age = 25; 
+
+// Question no 2
+alert("my age is  " + age);
+
+var NO = 14; 
+
+alert ("You have visited this site" + NO +" times");
+
+// Question no 3
+
+var year = 2006; 
+document.write("My Birthday Year is ", year,"<br>");
+// Question no 4
+var visitorName = "John Doe"; 
+var productTitle = "T-shirt"; 
+var quantity = 5; 
+
+document.write(visitorName , " ordered " , quantity, productTitle , "(s) on XYZ Clothing store.");
